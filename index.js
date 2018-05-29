33f99ffb3e086012df2c4e9f8a618b3433f5d403
Parinaz Roghany <parinazroghany@yahoo.de>
2018-05-29 15:11:41 +0200

    add middleware for https

--- index.js
+++ index.js
@@ -1,95 +1,99 @@
-const express = require("express");
-const http = require("http");
+const express = require( "express" );
+const http = require( "http" );
 const app = express();
 let Token;
-if (process.env.Token) {
-  Token = process.env.Token;
+if ( process.env.Token ) {
+	Token = process.env.Token;
 } else {
-  Token = require("./secrets.json").Token;
+	Token = require( "./secrets.json" ).Token;
 }
-console.log("TOKEN", Token);
+console.log( "TOKEN", Token );
 
-const request = require("request");
-const hb = require("express-handlebars");
+const request = require( "request" );
+const hb = require( "express-handlebars" );
 
-app.engine("handlebars", hb());
+app.engine( "handlebars", hb() );
 
-app.set("view engine", "handlebars"); //diese beiden Zeilen bleiben immer gleich für wenn man Handlebars benutzt
+app.set( "view engine", "handlebars" ); //diese beiden Zeilen bleiben immer gleich für wenn man Handlebars benutzt
 
-app.use(express.static(__dirname + "/public"));
+app.use( express.static( __dirname + "/public" ) );
 
-// if (process.env.NODE_ENV == 'production' && !req.headers['x-forwarded-proto'].startsWith('http')) {
-//
-//         return res.redirect(`https://${req.hostname}${req.url}`);
-//
-//     }
+app.use( ( req, res, next ) => {
 
-app.get("/", function(req, res, next) {
-  res.redirect("/spaetifinder");
-});
+	if ( process.env.NODE_ENV == 'production' && !req.headers[ 'x-forwarded-proto' ].startsWith( 'https' ) ) {
 
-app.get("/spaetifinder", function(req, res) {
-  res.render("spaetifinder", {
-    title: "Spaetifinder"
-  });
-});
+		return res.redirect( `https://${req.hostname}${req.url}` );
 
-app.get("/addspaeti", function(req, res) {
-  res.render("addspaeti", {
-    title: "Spaetifinder"
-  });
-});
+	} else {
+		next();
+	}
+} )
 
-app.get("/results", function(req, res) {
-  var qs = {
-    action: "list",
-    apitoken: Token,
-    lat: req.query.lat,
-    long: req.query.long,
-    distance: req.query.distance
-  };
-  var request = require("request");
-  request(
-    {
-      uri: "http://m.spätifinder.de/apiv2",
-      qs
-    },
-    function(error, response, body) {
-      res.json(JSON.parse(body));
-    }
-  );
-});
 
-app.get("/list", (req, res, next) => {
-  console.log("REQ QUERY", req.query);
-  var qs = {
-    action: "list",
-    apitoken: Token,
-    lat: req.query.lat,
-    long: req.query.long,
-    distance: req.query.distance
-  };
-  if (!req.query.lat) {
-    return null;
-  }
-  var request = require("request");
-  request(
-    {
-      uri: "http://m.spätifinder.de/apiv2",
-      qs
-    },
-    (error, response, body) => {
-      // console.log("BODYYYYYYYYYYYYYYYY", JSON.parse(body));
-      var spaetis = JSON.parse(body);
-      console.log(spaetis[1]);
-      // for (var i = 0; i < spaetis.length; i++) {
-      // }
+app.get( "/", function( req, res, next ) {
+	res.redirect( "/spaetifinder" );
+} );
 
-      res.json(spaetis);
-    }
-  );
-});
+app.get( "/spaetifinder", function( req, res ) {
+	res.render( "spaetifinder", {
+		title: "Spaetifinder"
+	} );
+} );
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`SPAETIFINDER!!!!!!!!!`);
-});
+app.get( "/addspaeti", function( req, res ) {
+	res.render( "addspaeti", {
+		title: "Spaetifinder"
+	} );
+} );
+
+app.get( "/results", function( req, res ) {
+	var qs = {
+		action: "list",
+		apitoken: Token,
+		lat: req.query.lat,
+		long: req.query.long,
+		distance: req.query.distance
+	};
+	var request = require( "request" );
+	request( {
+			uri: "http://m.spätifinder.de/apiv2",
+			qs
+		},
+		function( error, response, body ) {
+			res.json( JSON.parse( body ) );
+		}
+	);
+} );
+
+app.get( "/list", ( req, res, next ) => {
+	console.log( "REQ QUERY", req.query );
+	var qs = {
+		action: "list",
+		apitoken: Token,
+		lat: req.query.lat,
+		long: req.query.long,
+		distance: req.query.distance
+	};
+	if ( !req.query.lat ) {
+		return null;
+	}
+	var request = require( "request" );
+	request( {
+			uri: "http://m.spätifinder.de/apiv2",
+			qs
+		},
+		( error, response, body ) => {
+			// console.log("BODYYYYYYYYYYYYYYYY", JSON.parse(body));
+			var spaetis = JSON.parse( body );
+			console.log( spaetis[ 1 ] );
+			// for (var i = 0; i < spaetis.length; i++) {
+			// }
+
+			res.json( spaetis );
+		}
+	);
+} );
+
+app.listen( process.env.PORT || 8080, () => {
+	console.log( `SPAETIFINDER!!!!!!!!!` );
+} );
